8b07abf8f8cee2f5bc7fd491a88ebc72b45b4a84
saaamxzy <zhx024@ucsd.edu>
2016-10-29 17:01:50 -0700

    3 activities

--- app/src/main/java/com/group3/smartshop/MainActivity.java
+++ app/src/main/java/com/group3/smartshop/MainActivity.java
@@ -1,13 +1,39 @@
 package com.group3.smartshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "group3.CSE110smartshop.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_online_search);
+        layout.addView(textView);
+
     }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, OnlineSearchActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/group3/smartshop/OnlineSearchActivity.java
@@ -0,0 +1,13 @@
+package com.group3.smartshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OnlineSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_online_search);
+    }
+}
