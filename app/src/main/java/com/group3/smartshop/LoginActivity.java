90748148a7e80980bd1084c4531e4fa49455f8d2
yil364 <yil364@ucsd.edu>
2016-11-11 00:10:11 -0800

    image name change...

--- app/src/main/java/com/group3/smartshop/LoginActivity.java
+++ app/src/main/java/com/group3/smartshop/LoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
@@ -12,7 +11,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
