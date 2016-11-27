f5127e7e87ac53edfdcae53aed93c9c3d2404177
Xiaolong Zhou <xiz266@ucsd.edu>
2016-11-26 18:21:15 -0800

    Add more profile scenario test

---
+++ app/src/androidTest/java/com/group3/smartshop/ChangepasswordResetEmailTest.java
@@ -0,0 +1,72 @@
+package com.group3.smartshop;
+
+/*  Scenario Test:
+ *      Given user profile interface,
+ *      when i click on "Send Password Reset Email",
+ *      Then App will require me to enter a email, and i will reset an email to reset my password.
+ */
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class ChangepasswordResetEmailTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void changepasswordResetEmailTest() {
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.email), isDisplayed()));
+        appCompatEditText.perform(replaceText("xiz266@ucsd.edu"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.password), isDisplayed()));
+        appCompatEditText2.perform(replaceText("Xl.3635"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btn_login), withText("LOGIN"), isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Open navigation drawer"),
+                        withParent(allOf(withId(R.id.toolbar),
+                                withParent(withId(R.id.app_bar)))),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction appCompatCheckedTextView = onView(
+                allOf(withId(R.id.design_menu_item_text), withText("Search"), isDisplayed()));
+        appCompatCheckedTextView.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.login), withText("Profile"), isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.sending_pass_reset_button), withText("Send Password reset email"), isDisplayed()));
+        appCompatButton3.perform(click());
+
+    }
+
+}
