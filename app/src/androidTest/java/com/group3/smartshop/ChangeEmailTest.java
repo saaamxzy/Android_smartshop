90ddb05bdb3cd321582818244a0228524ef6fb48
Xiaolong Zhou <xiz266@ucsd.edu>
2016-11-30 23:33:17 -0800

    Update Scenario TEST

--- app/src/androidTest/java/com/group3/smartshop/ChangeEmailTest.java
+++ app/src/androidTest/java/com/group3/smartshop/ChangeEmailTest.java
@@ -10,14 +10,7 @@ import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,9 +18,7 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
 import static android.support.test.espresso.action.ViewActions.replaceText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -52,10 +43,6 @@ public class ChangeEmailTest {
                 allOf(withId(R.id.password), isDisplayed()));
         appCompatEditText2.perform(replaceText("Xl.3635"), closeSoftKeyboard());
 
-        ViewInteraction appCompatEditText3 = onView(
-                allOf(withId(R.id.password), withText("Xl.3635"), isDisplayed()));
-        appCompatEditText3.perform(pressImeActionButton());
-
         ViewInteraction appCompatButton = onView(
                 allOf(withId(R.id.btn_login), withText("LOGIN"), isDisplayed()));
         appCompatButton.perform(click());
@@ -68,44 +55,13 @@ public class ChangeEmailTest {
         appCompatImageButton.perform(click());
 
         ViewInteraction appCompatCheckedTextView = onView(
-                allOf(withId(R.id.design_menu_item_text), withText("Search"), isDisplayed()));
+                allOf(withId(R.id.design_menu_item_text), withText("My Profile"), isDisplayed()));
         appCompatCheckedTextView.perform(click());
 
         ViewInteraction appCompatButton2 = onView(
-                allOf(withId(R.id.profile_test), withText("Menu"), isDisplayed()));
+                allOf(withId(R.id.change_email_button), withText("Change Email"), isDisplayed()));
         appCompatButton2.perform(click());
 
-        ViewInteraction button = onView(
-                allOf(withId(R.id.change_email_button),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class),
-                                        2),
-                                1),
-                        isDisplayed()));
-
-        ViewInteraction appCompatButton3 = onView(
-                allOf(withId(R.id.change_email_button), withText("Change Email"), isDisplayed()));
-        appCompatButton3.perform(click());
-
     }
 
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
-    }
 }
